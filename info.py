986db1cea01cbd5c39e472b81956e41b1a9f2e28
Rohitcoder12 <rohitgandu91@gmail.com>
2024-11-02 17:53:43 +0530

    Update info.py

--- info.py
+++ info.py
@@ -24,7 +24,7 @@ BOT_TOKEN = environ.get('BOT_TOKEN', "")
 
 # Bot settings
 CACHE_TIME = int(environ.get('CACHE_TIME', 1800))
-PICS = (environ.get('PICS', 'https://graph.org/file/7a97a7bbc11becbf7e207.jpg')).split() #SAMPLE PIC
+PICS = (environ.get('PICS', 'https://ibb.co/jh0w4vJ')).split() #SAMPLE PIC
 NOR_IMG = environ.get("NOR_IMG", "https://ibb.co/nznLgFs")
 MELCOW_VID = environ.get("MELCOW_VID", "https://t.me/dailynewswallachat/424")
 SPELL_IMG = environ.get("SPELL_IMG", "https://te.legra.ph/file/15c1ad448dfe472a5cbb8.jpg")
